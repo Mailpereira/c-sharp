a29175f005304d721ce99140112c6d7c3826dc4b
Mailson Pereira <fmd.pereir@gmail.com>
2021-08-24 21:13:02 -0300

    nova mensagem

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -8,6 +8,7 @@ namespace OlaMundo
         {
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Respeitar pai e mãe");
+            Console.WriteLine("Eu nasci para ter mais");
         }
     }
 }
