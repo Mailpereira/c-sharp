fcc53ac5a03fe0aef66e74b16f09e4bff7224f89
Mailson Pereira <fmd.pereir@gmail.com>
2021-08-24 22:33:51 -0300

    alterando as msg

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -8,7 +8,7 @@ namespace OlaMundo
         {
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Respeitar pai e mãe");
-            Console.WriteLine("Eu nasci para ter mais");
+            Console.WriteLine("Foco nos estudos");
             Console.WriteLine("testando o git");
         }
     }
