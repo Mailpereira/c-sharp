c947989ec192e512cf1ea07a383b918a6373e372
Mailson Pereira <fmd.pereir@gmail.com>
2021-08-24 22:31:19 -0300

    testando recursos do git

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -9,6 +9,7 @@ namespace OlaMundo
             Console.WriteLine("Olá Mundo!");
             Console.WriteLine("Respeitar pai e mãe");
             Console.WriteLine("Eu nasci para ter mais");
+            Console.WriteLine("testando o git");
         }
     }
 }
